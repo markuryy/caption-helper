e9a6ead56adfe0e025e61f55961110803bf4c300
mark <mr.catborg@gmail.com>
2024-07-12 11:33:50 -0400

    add docs

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,16 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Markury Project",
-  description: "A project by Markury",
+  name: "Caption Helper",
+  description: "A simple tool to assist in creating and enhancing image/text pairs.",
   navItems: [
     {
       label: "Captioning Tools",
       href: "/",
+    },
+    {
+      label: "Docs",
+      href: "https://github.com/markuryy/caption-helper/blob/main/README.md",
     },
   ],
   navMenuItems: [
@@ -14,11 +18,15 @@ export const siteConfig = {
       label: "Captioning Tools",
       href: "/",
     },
+    {
+      label: "Docs",
+      href: "https://github.com/markuryy/caption-helper/blob/main/README.md",
+    },
   ],
   links: {
     github: "https://github.com/markuryy",
     twitter: "https://twitter.com/markuryy",
-    docs: "/docs",
+    docs: "https://github.com/markuryy/caption-helper/blob/main/README.md",
     discord: "https://discord.gg/YNtAWS7KRM",
     sponsor: "https://patreon.com/MarkuryAI",
   },
